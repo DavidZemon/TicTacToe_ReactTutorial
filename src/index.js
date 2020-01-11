144d8bbf38fe2a7732df5016d639578625b73e68
David Zemon <david@zemon.name>
2020-01-11 10:50:08 -0600

    Display winner and lock game

--- src/index.js
+++ src/index.js
@@ -24,7 +24,8 @@ class Board extends React.Component {
     this.state = {
       turn: 0,
       turns: [],
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      winner: null
     }
   }
 
@@ -38,7 +39,7 @@ class Board extends React.Component {
   }
 
   handleClick(i) {
-    if (this.state.squares[i] == null) {
+    if (null == this.state.squares[i] && null == this.state.winner) {
       // Make note of the move
       const turns = this.state.turns.slice();
       turns.push(new Move(i, this.currentPlayer()));
@@ -49,12 +50,12 @@ class Board extends React.Component {
       squares[i] = this.currentPlayer();
       this.setState({turn: this.state.turn + 1});
       this.setState({squares: squares});
+      this.setState({winner: Board.calculateWinner(squares)});
     }
   }
 
   render() {
-    const status = `Next player: ${(this.currentPlayer())}`;
-
+    const status = this.state.winner ? `Winner: ${this.state.winner}` : `Next player: ${this.currentPlayer()}`;
     return (
       <div>
         <div className="status">{status}</div>
@@ -73,6 +74,26 @@ class Board extends React.Component {
 
   currentPlayer() {
     return this.players[this.state.turn % 2];
+  }
+
+  static calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 }
 
